96d0fd5abff20355182135fe09b3f7f1b33ece51
sophia <sophiasiliu@gmail.com>
2025-01-25 14:05:41 -0500

    Update userinput.py

--- userinput.py
+++ userinput.py
@@ -1,6 +1,11 @@
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
+import streamlit as st
+import streamlit.components.v1 as components
 
+
+
+st.title("Welcome to PB & Jam!")
 
 df= pd.read_csv("spotify_songs.csv").dropna()
 #unique_tracks = [df['track_name'].drop_duplicates().reset_index(drop=True)]
@@ -9,24 +14,33 @@ df.drop_duplicates(subset = ['track_name'], inplace = True)
 
 X = df[['energy', 'tempo', 'danceability', 'valence', 'instrumentalness']].values
 
-song = "She Looks So Perfect" #input("Enter song name: ")
-artist = "5 Seconds of Summer" #input("Enter song artist: ")
+song = "Shake It Off" #input("Enter song name: ")
+artist = "Taylor Swift" #input("Enter song artist: ")
 #trackexists = ((df['track_name'] == song) & (df['track_artist'] == artist)).any()
 
 songfeatures = df[(df['track_name'].str.lower() == song.lower()) & (df['track_artist'].str.lower() == artist.lower())][["track_name", "track_artist", "energy", "tempo", "danceability", "valence", "instrumentalness"]].head(1)
 
 if songfeatures.empty:
-    raise Exception("Still cooking, stay tuned!")
+    st.subheader("We're still cooking, stay tuned!")
 else:
     songvector = [songfeatures.iloc[0][['energy', 'tempo', 'danceability', 'valence', 'instrumentalness']].values]
     similarityScores = cosine_similarity(songvector, X)
     similarIndices = similarityScores.argsort()[0][::-1]
-    topN = 20
-    topSongs = df.iloc[similarIndices[1:topN+1]]
-    print(topSongs)
-    trackIDs = list(dict.fromkeys(topSongs['track_id'].values))
-    links = []
-    for trackID in trackIDs:
-        link = 'https://open.spotify.com/track/' + str(trackID)
-        print(link)
-        links.append(link)
+    topN = 10
+    n = 1
+    while True: 
+        topSongs = df.iloc[similarIndices[n:topN+1]]
+        print(topSongs)
+        songNames = list(dict.fromkeys(topSongs['track_name'].values))
+        artistNames = list(dict.fromkeys(topSongs['track_artist'].values))
+        trackIDs = list(dict.fromkeys(topSongs['track_id'].values))
+        st.subheader("Playlist Bot: Here's your jam, enjoy!")
+        for i in range(len(trackIDs)):
+            link = "https://open.spotify.com/embed/track/" + str(trackIDs[i])
+            iframe_src = link
+            components.iframe(iframe_src, height=175, scrolling=True)
+        refresh = st.button("refresh")
+        if not refresh: 
+            break
+        else:
+            n = 11
