dbe2f5f6c47d006d302b0eb1d30bbd9e79d5f5fc
charlotte-em-smith <158656731+charlotte-em-smith@users.noreply.github.com>
2025-01-25 20:57:00 -0500

    Update 3_Find_a_Song_With_a_Similar_Vibe.py
    Updated with new dataset and with graphs

--- pages/3_Find_a_Song_With_a_Similar_Vibe.py
+++ pages/3_Find_a_Song_With_a_Similar_Vibe.py
@@ -2,71 +2,95 @@ import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 import streamlit as st
 import streamlit.components.v1 as components
-import plotly.express as px
+import plotly.express as px #need to pip install plotly
 
+#page set up with icon
 st.set_page_config(
     page_title="PB & Jam",
     page_icon="🍞",
 )
-
 st.title("Find a Song With a Similar Vibe")
 
-df= pd.read_csv("spotify_songs.csv").dropna()
-df.drop_duplicates(subset = ['track_name'], inplace = True)
-
+#dataset from new csv file
+df = pd.read_csv("spotify_songs1.csv", sep=',')
+df = df[['track_id', 'track_name', 'track_artist', 'energy', 'tempo', 'danceability', 'valence', 'instrumentalness']].dropna()
+df.drop_duplicates(subset = ['track_name', 'track_artist'], inplace = True)
 X = df[['energy', 'tempo', 'danceability', 'valence', 'instrumentalness']].values
 
-song = "Shake It Off" #input("Enter song name: ")
-artist = "Taylor Swift" #input("Enter song artist: ")
-#trackexists = ((df['track_name'] == song) & (df['track_artist'] == artist)).any()
+#get song as user input
+song = st.selectbox(label='Input Your Song Here!', options=df[['track_name']], index=None)
 
-songfeatures = df[(df['track_name'].str.lower() == song.lower()) & (df['track_artist'].str.lower() == artist.lower())][["track_name", "track_artist", "energy", "tempo", "danceability", "valence", "instrumentalness"]].head(1)
-
-if songfeatures.empty:
-    st.subheader("We're still cooking, stay tuned!")
+#for when the user hasn't input anything, playlist button
+if song == None:
+    refresh = st.button("create new playlist", type="secondary", icon=':material/refresh:', use_container_width=True)
 else:
-    songvector = [songfeatures.iloc[0][['energy', 'tempo', 'danceability', 'valence', 'instrumentalness']].values]
-    similarityScores = cosine_similarity(songvector, X)
-    similarIndices = similarityScores.argsort()[0][::-1]
-    topN = 10
+    #create songfeatures for user input
+    songfeatures = df[(df['track_name'].str.lower() == song.lower())][["track_name", "track_artist", "energy", "tempo", "danceability", "valence", "instrumentalness"]].head(1)
 
-    if ('n' not in st.session_state):
-        st.session_state['n'] = -9
+    #if songfeatures empty, display loading message
+    if songfeatures.empty:
+        st.subheader("We're still cooking, stay tuned!")
+    else:
+        #obtain info and compolete cosine comparision
+        songvector = [songfeatures.iloc[0][['energy', 'tempo', 'danceability', 'valence', 'instrumentalness']].values]
+        similarityScores = cosine_similarity(songvector, X)
+        similarIndices = similarityScores.argsort()[0][::-1]
 
-    if ('topN' not in st.session_state):
-        st.session_state['topN'] = 0
-    
-    refresh = st.button("refresh", type="secondary", icon=':material/refresh:', use_container_width=True)
-    if (refresh):
-        #st.balloons()
-        st.session_state['n'] += 10
-        n = st.session_state['n']
-        st.session_state['topN'] += 10
-        topN = st.session_state['topN']
-        topSongs = df.iloc[similarIndices[n:topN+1]]
-        print(topSongs)
-        songNames = list(dict.fromkeys(topSongs['track_name'].values))
-        artistNames = list(dict.fromkeys(topSongs['track_artist'].values))
-        trackIDs = list(dict.fromkeys(topSongs['track_id'].values))
-        st.subheader("Playlist Bot: Here's your jam, enjoy!")
-        for i in range(len(trackIDs)):
-            link = "https://open.spotify.com/embed/track/" + str(trackIDs[i])
-            iframe_src = link
-            components.iframe(iframe_src, height=175, scrolling=True)
-        #st.toast('Done!',icon=':material/check_box:')
-        tab1, tab2, tab3, tab4, tab5 = st.tabs(["Energy", "Tempo", "Danceability", "Valence", "Instrumentalness"])
-        with tab1:
-            fig = px.scatter(topSongs,x='track_name',y="energy")
-            st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-        with tab2:
-            fig = px.scatter(topSongs,x='track_name',y="tempo")
-            st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-        with tab3:
-            fig = px.scatter(topSongs,x='track_name',y="danceability")
-            st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-        with tab4:
-            fig = px.scatter(topSongs,x='track_name',y="valence")
-            st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-        with tab5:
-            fig = px.scatter(topSongs,x='track_name',y="instrumentalness")
-            st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+        #if not in state, set to negative (before user interaction)
+        if ('n' not in st.session_state):
+            st.session_state['n'] = -9
+        #if not in state, set to 0 (before user interaction)
+        if ('topN' not in st.session_state):
+            st.session_state['topN'] = 0
+        
+        #create playlist button again
+        refresh = st.button("create new playlist", type="secondary", icon=':material/refresh:', use_container_width=True)
+        #if clicked
+        if (refresh):
+            #st.balloons()
+            #for each click of refresh, add 10 to n and topN (to cycle through new songs)
+            st.session_state['n'] += 10
+            n = st.session_state['n']
+            st.session_state['topN'] += 10
+            topN = st.session_state['topN']
+
+            #top n songs from cosine comparision
+            topSongs = df.iloc[similarIndices[n:topN+1]]
+            #define vars (may not be needed)
+            songNames = list(dict.fromkeys(topSongs['track_name'].values))
+            artistNames = list(dict.fromkeys(topSongs['track_artist'].values))
+            trackIDs = list(dict.fromkeys(topSongs['track_id'].values))
+
+            #display playlist
+            st.subheader("Playlist Bot: Here's your jam, enjoy!")
+            #loop through given ids for cosine similar topN songs, and embed them with spotify
+            for i in range(len(trackIDs)):
+                link = "https://open.spotify.com/embed/track/" + str(trackIDs[i])
+                iframe_src = link
+                components.iframe(iframe_src, height=175, scrolling=True)
+            #st.toast('Done!',icon=':material/check_box:')
+
+            #display visual representation of cosine comparision (eg. energy for each song given)
+            st.subheader("Our Secret Recipe for Your Jam:")
+            #tabs to switch between representations (like switch statement)
+            tab1, tab2, tab3, tab4, tab5 = st.tabs(["Energy", "Tempo", "Danceability", "Valence", "Instrumentalness"])
+            with tab1:
+                #figure for energy
+                fig = px.scatter(topSongs,x='track_name',y="energy", color="playlist_genre", labels={"track_name": "Track Name", "energy": "Energy", "playlist_genre": "Playlist Genre"})
+                st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+            with tab2:
+                #figure for tempo
+                fig = px.scatter(topSongs,x='track_name',y="tempo", color="playlist_genre", labels={"track_name": "Track Name", "tempo": "Tempo", "playlist_genre": "Playlist Genre"})
+                st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+            with tab3:
+                #figure for danceability
+                fig = px.scatter(topSongs,x='track_name',y="danceability", color="playlist_genre", labels={"track_name": "Track Name", "danceability": "Danceability", "playlist_genre": "Playlist Genre"})
+                st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+            with tab4:
+                #figure for valence
+                fig = px.scatter(topSongs,x='track_name',y="valence", color="playlist_genre", labels={"track_name": "Track Name", "valence": "Valence", "playlist_genre": "Playlist Genre"})
+                st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+            with tab5:
+                #figure for intrumentalness
+                fig = px.scatter(topSongs,x='track_name',y="instrumentalness", color="playlist_genre", labels={"track_name": "Track Name", "instrumentalness": "Instrumentalness", "playlist_genre": "Playlist Genre"})
+                st.plotly_chart(fig, theme="streamlit", use_container_width=True)
