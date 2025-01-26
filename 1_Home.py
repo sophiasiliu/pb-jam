72f44c79786e4a5701f7f09b7b8d02aa313cf5ad
charlotte-em-smith <158656731+charlotte-em-smith@users.noreply.github.com>
2025-01-25 21:16:58 -0500

    Update 1_Home.py
    Added tabs for spotify viewing and dataframe viewing of dataset

--- 1_Home.py
+++ 1_Home.py
@@ -3,12 +3,13 @@ import time
 import pandas as pd
 import streamlit.components.v1 as components
 
+#page set up with item
 st.set_page_config(
     page_title="PB & Jam",
     page_icon="🍞",
 )
-
 st.title("Welcome to PB & Jam!")
+
 
 _ABOUT_US = """ A bit about us... like... erm... stuff and... the like? yeah. that sounds legit. """
 def stream_data():
@@ -17,23 +18,17 @@ def stream_data():
         time.sleep(0.02)
 
 st.write_stream(stream_data)
-
 st.write("Choose an option below to get started:")
 
-# Create two columns
-col1, col2 = st.columns(2)
+# Buttons for navigation
+if st.button("Generate a Playlist Based on Your Mood"):
+    # Navigate to the "Generate a Playlist" page
+    st.switch_page(page="pages/2_Generate_a_Playlist_Based_on_Your_Mood.py")
 
-# Button in the first column
-with col1:
-    if st.button("Create Your Own Jam"):
-        # Navigate to the "Generate a Playlist" page
-        st.switch_page(page="pages/2_Create_Your_Own_Jam.py")
+if st.button("Find a Song With a Similar Vibe"):
+    # Navigate to the "Find a Similar Song" page
+    st.switch_page(page="pages/3_Find_a_Song_With_a_Similar_Vibe.py")
 
-# Button in the second column
-with col2:
-    if st.button("Find a Song With a Similar Vibe"):
-        # Navigate to the "Find a Similar Song" page
-        st.switch_page(page="pages/3_Find_a_Song_With_a_Similar_Vibe.py")
 
 #load dataset
 df = pd.read_csv("spotify_songs1.csv", sep=',')
@@ -44,25 +39,30 @@ df.drop_duplicates(subset = ['track_name', 'track_artist'], inplace = True)
 st.subheader("Our Available Ingredients:")
 trackIDs = list(dict.fromkeys(df['track_id'].values))
 
-#set true to have initial 3 songs
-load_more = True
+#tabs for dataset viewing
+tab1, tab2 = st.tabs(["Dataframe View", "Spotify View"])
+with tab1:
+    #tab for dataset viewing
+    st.dataframe(df[["track_name", "track_artist", "energy", "tempo", "danceability", "valence", "instrumentalness"]])
+with tab2:
+    #tab for spotify viewing
+    #set true to have initial 3 songs
+    load_more = True
 
-#set n to 0 if not in session
-if ('n' not in st.session_state):
-    st.session_state['n'] = 0
+    #set n to 0 if not in session
+    if ('n' not in st.session_state):
+        st.session_state['n'] = 0
 
-#if load_more is clicked (or true)
-if(load_more):
-    #n = session state
-    st.session_state['n'] += 3
-    n = st.session_state['n']
-    #change existing 3 songs to new dataset (embedded spotify)
-    for i in range(n,  n + 3): 
-        link = "https://open.spotify.com/embed/track/" + str(trackIDs[i])
-        iframe_src = link
-        components.iframe(iframe_src, height=175, scrolling=True)
+    #if load_more is clicked (or true)
+    if(load_more):
+        #n = session state
+        st.session_state['n'] += 3
+        n = st.session_state['n']
+        #change existing 3 songs to new dataset (embedded spotify)
+        for i in range(n,  n + 3): 
+            link = "https://open.spotify.com/embed/track/" + str(trackIDs[i])
+            iframe_src = link
+            components.iframe(iframe_src, height=175, scrolling=True)
 
-#place load (under songs)
-load_more = st.button("Load More", type="secondary", icon=':material/refresh:', use_container_width=True)
-
-
+    #place load (under songs)
+    load_more = st.button("Load More", type="secondary", icon=':material/refresh:', use_container_width=True)
