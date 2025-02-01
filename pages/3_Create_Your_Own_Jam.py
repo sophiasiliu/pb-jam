3f9168f4132bc46e9c9090ab5d2d5f9a5e9267e5
sophia <sophiasiliu@gmail.com>
2025-02-01 13:55:17 -0500

    Update 3_Create_Your_Own_Jam.py

--- pages/3_Create_Your_Own_Jam.py
+++ pages/3_Create_Your_Own_Jam.py
@@ -20,9 +20,6 @@ df = df[['track_id', 'track_name', 'track_artist', 'energy', 'tempo', 'danceabil
 df.drop_duplicates(subset = ['track_name', 'track_artist'], inplace = True)
 X = df[['energy', 'tempo', 'danceability', 'valence', 'instrumentalness']].values
 
-os.environ["OPENAI_API_KEY"] = "" #put your API key here!
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
 
 def get_feature_estimates(prompt):
     try:
